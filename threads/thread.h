ef9ff7df0aa4a3216208d74f139cb7ea5b87cd6d
seolan <seolan25@gmail.com>
2024-11-16 21:35:29 +0900

    Proj3: Start project 3

--- threads/thread.h
+++ threads/thread.h
@@ -97,6 +97,9 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    // Alarm-Clock
+  	int64_t alarm_time;
+
    #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
